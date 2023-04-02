46f8d2faa15fe18216b227eca364945ec417dec2
Veera <nmani0303@hotmail.com>
2023-04-02 21:17:19 +0800

    yml path change

--- src/utils/td.js
+++ src/utils/td.js
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
 let getTd = async (key) => {
-    let yamlFile = fs.readFileSync('/Users/natarajanveeramanikandan/dev-space/qa-wdio-js/src/data/data.yml', "utf8");
+    let yamlFile = fs.readFileSync('src/data/data.yml', "utf8");
     let td = await yaml.load(yamlFile);
     return td[key];
 }
